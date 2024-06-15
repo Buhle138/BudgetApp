0bd8beb96ce76b3b839173e4728bc7bc92a32505
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-15 23:39:29 +0200

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Buhle Radzilani on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
